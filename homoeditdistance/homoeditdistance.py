2b03371fa419a777709519218c525579d8a4838e
Philipp Spohr <philipp.spohr@hhu.de>
2020-05-06 18:02:46 +0200

    - implemented some backtracking showing possible optimal operation sequences

--- homoeditdistance/homoeditdistance.py
+++ homoeditdistance/homoeditdistance.py
@@ -18,43 +18,28 @@ import sys, argparse
 
 
 # Main dynamic programming algorithm to compute the homo-edit distance between two strings s and t.
-def homoEditDistance(s, t):
+
+# backtracking can be controlled with int argument 0-> none, 1-> basic, 2-> full
+def homoEditDistance(s, t, backtracking = 0):
     m = len(s)
     n = len(t)
     d = np.zeros(shape=(m + 1, n + 1), dtype=object)
-    H = distancesToEmptyString(s)
-    H.update(distancesToEmptyString(t))
-    for i in range(0, len(s) + 1):
-        for j in range(0, len(t) + 1):
-            C = list([])
-            # initialisation
-            if i == 0 and j == 0:
-                d[i][j] = 0
-            # main body of method
-            else:
-                if i > 0 and j > 0:
-                    if s[i-1] == t[j-1]:
-                        C.append(d[i-1][j-1])
+    bt = np.zeros(shape=(m + 1, n + 1), dtype=object) if backtracking > 0 else None
 
-                for k in range (0,i):
-                    C.append(d[k,j] + H[(s, k, i-1)])
+    #Calculate auxiliary DP
+    auxResultS = distancesToEmptyString(s,backtracking)
+    H = auxResultS['H']
+    auxResultT = distancesToEmptyString(t,backtracking)
+    H_t = auxResultT['H']
+    H.update(H_t)
+    
+    #Fetch backtracking if applicable
+    zbt = {} if backtracking == 2 else None    
 
-                for l in range (0,j):
-                    C.append(d[i,l] + H[(t, l, j-1)])
+    if backtracking == 2:
+        zbt = auxResultS['BTMatrix']
+        zbt.update(auxResultT['BTMatrix'])
 
-                d[i][j] = int(min(C))
-
-    return d[m][n]
-
-
-
-def homoEditDistanceWbacktrack(s, t):
-    m = len(s)
-    n = len(t)
-    d = np.zeros(shape=(m + 1, n + 1), dtype=object)
-    bt = np.zeros(shape=(m + 1, n + 1), dtype=object)
-    H = distancesToEmptyString(s)
-    H.update(distancesToEmptyString(t))
     for i in range(0, len(s) + 1):
         for j in range(0, len(t) + 1):
             C = list([])
@@ -85,10 +70,19 @@ def homoEditDistanceWbacktrack(s, t):
                         C.append((i,l))
 
                 d[i][j] = c
-                bt[i][j] = list(C)
-    sol = backtrack(bt, s, t)
-    return d[m][n], sol
+                if backtracking > 0:
+                    bt[i][j] = list(C)
+    ret = {
+        'hed' : d[m][n]
+    }
+    
+    #Add backtracking matrices to result if needed
+    if backtracking > 0 :
+        ret['bt'] = bt
+    if backtracking == 2:
+        ret['zbt'] = zbt
 
+    return ret
 
 
 def backtrack(bt, s, t):
@@ -121,12 +115,102 @@ def backtrackRecursive(bt, s, t, sub, i, j):
             for gen in backtrackRecursive(bt, s, t, sub + s[i - 1], C[0], C[1]):
                 yield gen
 
+#Performs backtracking on the auxiliary dynamic programming method to resolve string deletions
+def resolveDeletion(s,btz,i,j):
+    if i==j:
+        return []
 
+    btd = btz[(s,i,j)]
+    for k in btd:
+        #TODO: Show multiple variants for deletions
+        if s[i] == s[j-1]:
+            return [(i,j)]+resolveDeletion(s,btz,i,k)+resolveDeletion(s,btz,k+1,j)
+        else:
+            return resolveDeletion(s,btz,i,k)+resolveDeletion(s,btz,k+1,j)
+     
+#Applies a list of deletions on a string and outputs human-readable representations of the deletion events
+def processDeletions(path,string,deletionInstructions):
+    extension = []
+    tmp = string
+    previousInstruction = None
+    while deletionInstructions:
+        currentInstruction = deletionInstructions.pop()
+        #print(deletionInstructions,currentInstruction)
+        start = currentInstruction[0]
+        end = currentInstruction[1]
+        deletion = tmp[start:end]
+        tmp = tmp[:start]+tmp[end:]
+        extension.append(' Deleting: {} ... \n Result: {}'.format(deletion,tmp))
+        length = end-start
+        for idx,inst in enumerate(deletionInstructions):
+            #check events that overlap
+            if inst[0] <= start and inst[1] >= end:
+                deletionInstructions[idx] = (inst[0],inst[1]-length)
+    path += extension#[::-1]
+        
+#Replaces all deletion events with a step by step backtracking
+def resolveDeletions(path,s,t,btz):
+    newPath = ['s: {} t: {}'.format(s,t)]
+    #the state of the strings at a given location in the path is reflected in smod and tmod
+    smod = s
+    tmod = t
+    for step in path:
+        stepData = step.split(' ')
+        if stepData[0] == 'del':
+            string = s if stepData[1] == 's' else t
+            i = int(stepData[3])
+            j = int(stepData[2])
+            deletionInstructions = resolveDeletion(string,btz,i,j)#[::-1]
+            #print(deletionInstructions)
+            if stepData[1] == 's':
+                newPath.append('Deleting substring {} -> {} ({}) from s'.format(i,j,s[i:j]))
+                smod = processDeletions(newPath,smod,deletionInstructions) 
+            else:
+                newPath.append('Deleting substring {} -> {} ({}) from t'.format(i,j,s[i:j]))
+                tmod = processDeletions(newPath,tmod,deletionInstructions)
+        else:
+            pass
+
+    return newPath
+
+# returns all possible backtracking paths that resulted in the calculated optimal homo-edit distance
+def assemblePaths(bt, s, t,btz):
+    transforms = []
+    m = len(s)
+    n = len(t)
+    for gen in assemblePathsRecursive(bt, s, t, transforms, m, n):
+        yield resolveDeletions(gen,s,t,btz)
+
+
+
+def assemblePathsRecursive(bt, s, t, transforms, i, j):
+    #print(i,j)
+    if i == 0 and j == 0:
+        yield transforms[::-1]
+        return
+    for C in bt[i][j]:
+        # horizontal
+        if C[0] == i:
+            for gen in assemblePathsRecursive(bt, s, t, transforms+['del t {} {}'.format(j,C[1])], i, C[1]):
+                yield gen
+        
+        # vertical
+        elif C[1] == j:
+
+            for gen in assemblePathsRecursive(bt, s, t, transforms+['del s {} {}'.format(i,C[0])], C[0], j):
+                yield gen
+            
+        # diagonal
+        else:
+            for gen in assemblePathsRecursive(bt, s, t, transforms+['match'], C[0], C[1]):
+                yield gen
 
 # Auxiliary dynamic programming algorithm to compute the homo-edit distance between every substring of a string s and the empty string.
-def distancesToEmptyString(s):
+def distancesToEmptyString(s,backtracking = 0):
     n = len(s)
     H = dict({})
+
+    BT = {} if backtracking == 2 else None
 
     for l in range(0, n):
         for i in range(0,n - l):
@@ -134,11 +218,24 @@ def distancesToEmptyString(s):
             if i == j:
                 H[(s, i, j)] = 1
             else:
-                C = list([])
+                #C = list([])
+                C = {}                
                 for k in range(i, j):
-                    C.append(H[(s, i, k)] + H[(s, k + 1, j)] - int(bool(s[i] == s[j])))
-                H[(s, i, j)] = int(min(C))
-    return H
+                    C[k] = (H[(s, i, k)] + H[(s, k + 1, j)] - int(bool(s[i] == s[j])))
+                H[(s, i, j)] = int(min(C.values()))
+                
+                if backtracking == 2:
+                    minKeys = [k for k in C if C[k] == H[(s,i,j)]]
+                    BT[(s,i,j)] = minKeys
+
+    ret =  {
+        'H' : H,
+    }
+
+    if backtracking == 2:
+        ret['BTMatrix'] = BT
+
+    return ret
 
 
 ## demo:
@@ -151,17 +248,38 @@ def get_parser():
     parser.add_argument('-s', '--string1', required=True, help='first string. Use \"STRING\" for the empty string or strings with special characters')
     parser.add_argument('-t', '--string2', required=True, help='second string')
     parser.add_argument('-a', '--all', action="store_true", default=False, required=False, help='show all optimal subsequences')
+    parser.add_argument('-b', '--backtrace', action="store_true", default=False, required=False, help='print transformation steps')
     return parser
 
 def run(args):
     s, t = args.string1, args.string2
-    if not args.all:
-        print("The homo-edit distance between", s, "and", t if t != "" else "the empty string", "is", homoEditDistance(s,t))
-    else:
-        print("The homo-edit distance between", s, "and", t if t != "" else "the empty string", "is ", end = '')
-        inst = homoEditDistanceWbacktrack(s, t)
-        print(inst[0], "with optimal common subsequences: ", end = '')
-        for sup in set(inst[1]): print(sup, end = ' ')
+
+    requiredBacktrackLevel = 0
+    if args.backtrace:
+        requiredBacktrackLevel = 2
+    elif args.all:
+        requiredBacktrackLevel = 1
+    
+    result = homoEditDistance(s,t,requiredBacktrackLevel)
+
+    print('The homo-edit distance between {} and {} is {}\n'.format(
+            s,t if t != "" else "the empty string",result['hed']
+        )
+    )
+
+    if args.all:
+        print('The following optimal subsequences were found:\n')
+        subs = backtrack(result['bt'],s,t)
+        for sup in set(subs): print(sup, end = ' ')
+        print('\n')#Needed?
+
+    if args.backtrace:
+            print('Detailed Backtracking:\n')
+            paths = assemblePaths(result['bt'], s, t,result['zbt'])
+            for idx,path in enumerate(paths):
+                print('Path {} (Possible optimal sequence of operations)\n'.format(idx))
+                for step in path:
+                    print(step)       
 
 if __name__ == "__main__":
     run(get_parser().parse_args(sys.argv[1:]))
