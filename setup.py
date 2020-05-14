3edf1ed630c5ef38ba1aa68cbca2208e90776261
Max Ried <max.ried@hhu.de>
2020-05-14 13:52:00 +0200

    Add license info to setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/AlBi-HHU/homo-edit-distance',
     packages=setuptools.find_packages(),
+    license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
