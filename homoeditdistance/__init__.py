9c4da52c1f7a188936f308cdc09433f3a8684c5a
Max Ried <max.ried@hhu.de>
2020-05-14 13:45:07 +0200

    Restructure of nearly everything. Now there are - hopefully - proper __-files, and the project is now split into algorithm.py and demonstration.py.

--- homoeditdistance/__init__.py
+++ homoeditdistance/__init__.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-all = ['homoEditDistance']
+"""
+Analyze two strings for their homo-edit distance.
+
+Implementation of the homo-edit-distance as described in
+add paper ref once it is accepted or location on some archive
+"""
+
+__author__ = 'Maren Brand, Gunnar W. Klau, Philipp Spohr, Nguyen Khoa Tran'
+__credits__ = ['Maren Brand', 'Gunnar W. Klau', 'Philipp Spohr', 'Nguyen Khoa Tran']
+__license__ = 'MIT'
+__copyright__ = 'Copyright (C) 2020 by AlBi-HHU (gunnar.klau@hhu.de),  all rights reserved, MIT license.'
+__email__ = 'gunnar.klau@hhu.de'
+# __revision__ = ''
+# __date__ = ''
+# __version__ = ''
+# __maintainer__ = ''
+# __status__ = ''
+
+from homoeditdistance.algorithm import homoEditDistance, backtrack, assemblePaths
+
+__all__ = ['homoEditDistance', 'backtrack', 'assemblePaths']
